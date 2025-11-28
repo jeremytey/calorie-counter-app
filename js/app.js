10494edbb0ffa6c74e9005a53c514c5a8840d009
jeremytey <jeremy8598@gmail.com>
2025-11-28 17:25:58 +0800

    initialize app.js pseudocode planning

--- js/app.js
+++ js/app.js
@@ -1,1 +1,22 @@
 // app.js -- entry point, orchestrates everything
+
+// app.js
+
+// Import statements
+// State object
+
+async function init() {
+  // Your pseudocode here
+}
+
+function handleSearch() {
+  // Your pseudocode here
+}
+
+function handleClear() {
+  // Your pseudocode here
+}
+
+function handleLoadMore() {
+  // Your pseudocode here
+}
